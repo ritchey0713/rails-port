780bd79bcb51772318f21b9f0e35c35184e4fbb1
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-13 20:34:54 -0500

    add update/edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,9 +26,20 @@ class PortfoliosController < ApplicationController
   end 
 
   def edit 
+    @portfolio_item = Portfolio.find(params[:id])
   end 
 
   def update 
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        #format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
   end 
 
   def destroy 
