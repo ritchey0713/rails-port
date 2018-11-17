32e4704d5c0b51b4fdb8e2a944b8c3138aec2030
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-17 14:26:52 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end 
+
+puts "create topics"
+
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    topic_id: Topic.last.id
     )
 end
 
@@ -16,15 +26,24 @@ end
 
 puts "5 skills created"
 
-9.times do |project|
+8.times do |project|
 Portfolio.create(
   title: "portfolio title #{project}",
-  subtitle: "my great project",
+  subtitle: "Ruby on Rails",
   body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
   main_image: "https://via.placeholder.com/600X400", 
   thumb_image: "https://via.placeholder.com/350X200"
 )
 end 
 
+2.times do |project|
+  Portfolio.create(
+    title: "portfolio title #{project}",
+    subtitle: "React",
+    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    main_image: "https://via.placeholder.com/600X400", 
+    thumb_image: "https://via.placeholder.com/350X200"
+  )
+  end 
 
 puts "9 portfolio projects created"
