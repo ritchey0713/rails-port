67db445d506f03902525d5bc1fc4e662b56d6c8b
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-17 14:27:30 -0500

    add new action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,11 @@
 class PortfoliosController < ApplicationController
 
   def index
-    @portfolio_items = Portfolio.all 
+    @portfolio_items = Portfolio.all
+  end
+  
+  def react 
+    @react_items = Portfolio.react
   end 
 
   def new 
