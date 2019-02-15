89f8a45561f104a0999d3942b604b3f0023da744
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2019-02-15 13:54:59 -0500

    added nest attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,6 +10,7 @@ class PortfoliosController < ApplicationController
 
   def new 
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end 
 
   def create 
@@ -59,7 +60,7 @@ class PortfoliosController < ApplicationController
   private
 
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:name])
     end
  
 end
