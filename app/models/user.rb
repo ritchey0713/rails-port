99b50bb475d95932ceb983933abbf2cd96f4dde7
Dj <Ritchey0713@gmail.com>
2019-02-16 13:52:24 -0500

    devise stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+validates_presence_of :user_name
+
+  def first_name 
+    self.user_name.split.first 
+  end 
+
+  def first_name 
+    self.user_name.split.last
+  end 
+
 end
