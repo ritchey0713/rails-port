108a8ac4683e82709fdb793771c7bfc255b23938
Dj <Ritchey0713@gmail.com>
2019-02-19 14:14:30 -0500

    fix strong params

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -60,7 +60,13 @@ class PortfoliosController < ApplicationController
   private
 
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:name])
+      params.require(:portfolio).permit(
+        :title, 
+        :subtitle, 
+        :body, 
+        :main_image, 
+        :thumb_image, 
+        technologies_attributes: [:name])
     end
  
 end
