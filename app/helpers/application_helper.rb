14878d79124317109a079d485122946bf4a76b05
Dj <Ritchey0713@gmail.com>
2019-02-24 15:35:49 -0500

    add source helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  
   def login_helper
     if current_user.is_a?(User) 
       link_to "Logout", destroy_user_session_path, method: :delete 
@@ -8,4 +9,11 @@ module ApplicationHelper
       (link_to "Register", new_user_registration_path) 
     end 
   end 
+
+  def source_helper(layout_name)
+    if session[:source]
+      content_tag(:p, "Thanks for visiting from #{session[:source]} and you came to the #{layout_name} layout", class: "source-greeting")
+    end 
+  end
+
 end
