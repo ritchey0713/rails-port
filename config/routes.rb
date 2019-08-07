ebb38234a1f9791a659629dfe39ee400fe6cdaa7
Dj <ritchey0713@gmail.com>
2019-08-07 12:31:57 -0400

    .

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'react-items', to: "portfolios#react"
   
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id', to: 'portfolios#show', as: "portfolio_show"
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
