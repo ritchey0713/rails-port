7eda2063938cf3699f60e487b527efd1a500c42c
Dj <ritchey0713@gmail.com>
2019-08-13 13:27:37 -0400

    add nav helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,59 @@ module ApplicationHelper
   def copyright_generator
     @copyright = RitcheySolutionViewtool::Renderer.copyright("Dj Ritchey", "All Rights Reserved")
   end 
-  
+
+  def nav_items 
+    [
+      {
+        url: root_path, 
+        title: "Home"
+      },
+      {
+        url: new_blog_path, 
+        title: "Create a blog"
+      },
+      {
+        url: blogs_path, 
+        title: "All blogs"
+      },
+      {
+        url: portfolios_path, 
+        title: "App portfolio"
+      },
+      {
+        url: about_me_path, 
+        title: "About me"
+      },
+      {
+        url: contact_path, 
+        title: "Contact"
+      },
+    ]
+  end 
+
+  def nav_helper(style, tag_type)
+    nav_links = ''
+    nav_items.each do |link|
+      nav_links << "<#{tag_type}><a href='#{link[:url]}' class='#{style} #{active?(link[:url])}'>#{link[:title]}</a></#{tag_type}>"
+
+    end 
+    nav_links.html_safe
+  end 
+
+#   def nav_helper(style, tag_type) 
+# nav_links = <<NAV 
+# <#{tag_type}><a href='#{root_path}' class="#{style} #{active?(root_path)}"> Home </a></#{tag_type}>
+# <#{tag_type}><a href='#{new_blog_path}' class="#{style} #{active?(new_blog_path)}"> Create a blog </a></#{tag_type}>
+# <#{tag_type}><a href='#{blogs_path}' class="#{style} #{active?(blogs_path)}"> See all blogs </a></#{tag_type}>
+# <#{tag_type}><a href='#{portfolios_path}' class="#{style} #{active?(portfolios_path)}"> App portfolio </a></#{tag_type}>
+# <#{tag_type}><a href='#{about_me_path}' class="#{style} #{active?(about_me_path)}"> About me </a></#{tag_type}>
+# <#{tag_type}><a href='#{contact_path}' class="#{style} #{active?(contact_path)}"> Contact us </a></#{tag_type}>
+# NAV
+# nav_links.html_safe
+#   end 
+
+  def active?(path)
+     "active" if current_page?(path) # current_page is rails method, will check if path is the current page
+  end
 
 end
