afd629c6b646ee4ae08918288ecdd6196b1fb7e8
Dj <ritchey0713@gmail.com>
2019-08-16 12:25:21 -0400

    write drag and drop func

--- app/assets/javascripts/portfolios.js
+++ app/assets/javascripts/portfolios.js
@@ -1,8 +1,59 @@
+const cards = document.getElementsByClassName("card");
+let updatedOrder = [];
+
+let setPositions = function(){
+    $(".card").each(function(ele){
+        $(this).attr('data-pos', ele + 1);
+        
+    });
+    // Array.prototype.forEach.call(cards, function(card){ how to use foreach on a nodeList
+    //     console.log(card)
+    // })
+};
+
+
+// let ready = function(){
+//     setPositions();
+//     $('.sortable').sortable();
+//     $('.sortable').sortable().bind('sortupdate');
+    
+//     setPositions();
+//     $('div[data-id]').each(function(ele){
+//         console.log($(this).data.id)
+//         updatedOrder.push(this);
+//     });
+//     $.ajax({
+//         type: "PUT",
+//         url: "/portfolios/sort",
+//         data: updatedOrder
+//     });
+// }; 
+
+// $(document).ready(ready);
+
 let ready = function(){
+    setPositions();
     $('.sortable').sortable();
-    console.log("test");
-}; 
+    $('.sortable').sortable().bind('sortupdate', function(event, ui){
+        setPositions();
+        $('div[data-id]').each(function(ele){
+            updatedOrder.push({
+                id: $(this).data("id"), 
+                position: ele + 1
+            });  
+        });
+        $.ajax({
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("X-CSRF-Token", $('meta[name="csrf-token"]').attr('content'))
+            },
+            type: "PUT",
+            url: "portfolios/sort",
+            data: {
+                order: updatedOrder
+            }
+        });
+    });
+};
+
 
 $(document).ready(ready);
-
-
