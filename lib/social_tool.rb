b0c29f3cab8b0e07e5eb108c7c826b2a5ace30f6
Dj <ritchey0713@gmail.com>
2019-09-10 11:40:16 -0400

    add social file

---
+++ lib/social_tool.rb
@@ -0,0 +1,14 @@
+ module SocialTool 
+    def self.twitter_search
+      client = Twitter::REST::Client.new do |config|
+         config.consumer_key        = ENV.fetch("CONSUMER_KEY")
+         config.consumer_secret     = ENV.fetch("CONSUMER_SECRET")
+         config.access_token        = ENV.fetch("ACCESS_TOKEN")
+         config.access_token_secret = ENV.fetch("ACCESS_SECRET")
+       end
+
+      client.search("#rails", result_type: 'recent').take(10).collect do |tweet|
+         "#{tweet.user.screen_name}: #{tweet.text}"
+      end 
+    end 
+ end 
