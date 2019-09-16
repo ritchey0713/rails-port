11d31333e52f300afb413846a5a1772e2c0fabfe
Dj <ritchey0713@gmail.com>
2019-09-16 14:12:33 -0400

    add create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
-end
+
+    def create 
+        @comment = current_user.comments.build(comment_params)
+    end
+
+
+
+private 
+
+    def comment_params
+        params.require(:comment).permit(
+            :content
+        )
+    end 
+
+end 
