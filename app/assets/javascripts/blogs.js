ac9b8b794ba2d780ff62e5c179afc176d95da7f8
Dj <ritchey0713@gmail.com>
2019-09-16 16:23:22 -0400

    cretae js file

---
+++ app/assets/javascripts/blogs.js
@@ -0,0 +1,39 @@
+let comments = $("#comments");
+
+let blogComments = function(){
+  if (comments.length > 0){
+    App.global_chat = App.cable.subscriptions.create(
+      {
+        channel: "BlogsChannel",
+        blogId: comments.data("blog-id")
+      },
+      {
+        connected: null,
+        disconneted: null,
+        received: function(data){
+          comments.append(data.comment);
+        },
+        sendComment: function(comment, blogId){
+          this.perform('send_comment', {
+            comment: comment,
+            blogId: blogId
+          });
+        },
+      }
+      
+      );
+  }
+  $('#new_comment').submit(function(e){
+    $this = $(this);
+    textArea = $this.find('#comment_content');
+    if ($.trim(textArea.val()).length > 1){
+      App.global_chat.send_comment(textArea.val(), comments.data('blog-id'));
+      textArea.val("");
+    }
+    e.preventDefault();
+    return false;
+  });
+};
+
+$(document).ready(blogComments);
+
