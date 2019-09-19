1c678a0ce72d13d791338f154a93ddfae350ab97
Dj <ritchey0713@gmail.com>
2019-09-19 16:19:15 -0400

    add private method

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_side_bar_topics, except: [:create, :update, :destroy, :toggle_status]
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
   layout "blog"
 
@@ -84,6 +85,10 @@ class BlogsController < ApplicationController
     def set_blog
       @blog = Blog.friendly.find(params[:id])
     end
+
+    def set_side_bar_topics 
+      @sidebar_topics = Topic.with_blogs
+    end 
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
