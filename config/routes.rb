78f986659061e21178d877c043ad75aae75a9663
Dj <ritchey0713@gmail.com>
2019-09-19 16:17:08 -0400

    add topics route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 
 Rails.application.routes.draw do
 
-  # resources :comments
+  resources :topics, only: [:show, :index, :create]
+
   devise_for :users, path: '', path_names: { sign_in: "login", sign_out: 'logout', sign_up: 'register' }
 
   resources :portfolios, except: [:show] do 
